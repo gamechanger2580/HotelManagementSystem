a8057dbaacd4d2f55bac4a5d89740d6dbbc2420b
Loukik Tayshete <loukik2580@gmail.com>
2025-12-21 13:32:50 +0530

    Inital Commit: Hotel Management Spring Boot

---
+++ src/main/java/com/gamechanger/project/hoteManagement/HoteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gamechanger.project.hoteManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamechanger/project/hoteManagement/HoteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamechanger.project.hoteManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
